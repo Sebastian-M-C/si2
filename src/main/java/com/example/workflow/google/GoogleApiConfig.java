d7838faf30cb599a92d4b3e1d8657fd64b16b017
luisb <luisbuceta060@gmail.com>
2024-11-27 12:04:43 -0600

    el inicio y cerrar ya da, ya puse mi parte, y lo de seba que dijo que le cambie le hice

--- src/main/java/com/example/workflow/google/GoogleApiConfig.java
+++ src/main/java/com/example/workflow/google/GoogleApiConfig.java
@@ -1,51 +1,51 @@
-package com.example.workflow.google;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.docs.v1.Docs;
-import com.google.api.services.drive.Drive;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.imageio.spi.IIORegistry;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Collections;
-
-@Configuration
-public class GoogleApiConfig {
-
-    private static final String APPLICATION_NAME = "GoogleDocsAPIIntegration";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance(); // Usa JacksonFactory
-    private static final String CREDENTIALS_FILE_PATH = "src/main/resources/google-credentials.json"; // Ruta al archivo de credenciales
-
-    @Bean
-    public Docs googleDocsService() throws Exception {
-        GoogleCredential credential = getGoogleCredential();
-        return new Docs.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JSON_FACTORY,
-                credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-
-    @Bean
-    public Drive googleDriveService() throws Exception {
-        GoogleCredential credential = getGoogleCredential();
-        return new Drive.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JSON_FACTORY,
-                credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-
-    private GoogleCredential getGoogleCredential() throws Exception {
-        InputStream credentialsStream = new FileInputStream(CREDENTIALS_FILE_PATH);
-        return GoogleCredential.fromStream(credentialsStream)
-                .createScoped(Collections.singletonList("https://www.googleapis.com/auth/drive"));
-    }
-}
+//package com.example.workflow.google;
+//
+//import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+//import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+//import com.google.api.client.json.JsonFactory;
+//import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.services.docs.v1.Docs;
+//import com.google.api.services.drive.Drive;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.imageio.spi.IIORegistry;
+//import java.io.FileInputStream;
+//import java.io.InputStream;
+//import java.util.Collections;
+//
+//@Configuration
+//public class GoogleApiConfig {
+//
+//    private static final String APPLICATION_NAME = "GoogleDocsAPIIntegration";
+//    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance(); // Usa JacksonFactory
+//    private static final String CREDENTIALS_FILE_PATH = "src/main/resources/google-credentials.json"; // Ruta al archivo de credenciales
+//
+//    @Bean
+//    public Docs googleDocsService() throws Exception {
+//        GoogleCredential credential = getGoogleCredential();
+//        return new Docs.Builder(
+//                GoogleNetHttpTransport.newTrustedTransport(),
+//                JSON_FACTORY,
+//                credential)
+//                .setApplicationName(APPLICATION_NAME)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Drive googleDriveService() throws Exception {
+//        GoogleCredential credential = getGoogleCredential();
+//        return new Drive.Builder(
+//                GoogleNetHttpTransport.newTrustedTransport(),
+//                JSON_FACTORY,
+//                credential)
+//                .setApplicationName(APPLICATION_NAME)
+//                .build();
+//    }
+//
+//    private GoogleCredential getGoogleCredential() throws Exception {
+//        InputStream credentialsStream = new FileInputStream(CREDENTIALS_FILE_PATH);
+//        return GoogleCredential.fromStream(credentialsStream)
+//                .createScoped(Collections.singletonList("https://www.googleapis.com/auth/drive"));
+//    }
+//}
--- src/main/java/com/example/workflow/google/GoogleServicesInitializer.java
+++ src/main/java/com/example/workflow/google/GoogleServicesInitializer.java
@@ -1,41 +1,41 @@
-package com.example.workflow.google;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.services.docs.v1.Docs;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collections;
-
-public class GoogleServicesInitializer {
-    private static final String APPLICATION_NAME = "Your App Name";
-
-    public static Docs getDocsService() throws IOException {
-        InputStream credentialsStream = GoogleServicesInitializer.class.getResourceAsStream("/google-credentials.json");
-
-        GoogleCredential credential = GoogleCredential.fromStream(credentialsStream)
-                .createScoped(Collections.singleton(DriveScopes.DRIVE));
-
-        return new Docs.Builder(
-                credential.getTransport(),
-                credential.getJsonFactory(),
-                credential
-        ).setApplicationName(APPLICATION_NAME).build();
-    }
-
-    public static Drive getDriveService() throws IOException {
-        InputStream credentialsStream = GoogleServicesInitializer.class.getResourceAsStream("/google-credentials.json");
-
-        GoogleCredential credential = GoogleCredential.fromStream(credentialsStream)
-                .createScoped(Collections.singleton(DriveScopes.DRIVE));
-
-        return new Drive.Builder(
-                credential.getTransport(),
-                credential.getJsonFactory(),
-                credential
-        ).setApplicationName(APPLICATION_NAME).build();
-    }
-
-}
+//package com.example.workflow.google;
+//
+//import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+//import com.google.api.services.docs.v1.Docs;
+//import com.google.api.services.drive.Drive;
+//import com.google.api.services.drive.DriveScopes;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.Collections;
+//
+//public class GoogleServicesInitializer {
+//    private static final String APPLICATION_NAME = "Your App Name";
+//
+//    public static Docs getDocsService() throws IOException {
+//        InputStream credentialsStream = GoogleServicesInitializer.class.getResourceAsStream("/google-credentials.json");
+//
+//        GoogleCredential credential = GoogleCredential.fromStream(credentialsStream)
+//                .createScoped(Collections.singleton(DriveScopes.DRIVE));
+//
+//        return new Docs.Builder(
+//                credential.getTransport(),
+//                credential.getJsonFactory(),
+//                credential
+//        ).setApplicationName(APPLICATION_NAME).build();
+//    }
+//
+//    public static Drive getDriveService() throws IOException {
+//        InputStream credentialsStream = GoogleServicesInitializer.class.getResourceAsStream("/google-credentials.json");
+//
+//        GoogleCredential credential = GoogleCredential.fromStream(credentialsStream)
+//                .createScoped(Collections.singleton(DriveScopes.DRIVE));
+//
+//        return new Drive.Builder(
+//                credential.getTransport(),
+//                credential.getJsonFactory(),
+//                credential
+//        ).setApplicationName(APPLICATION_NAME).build();
+//    }
+//
+//}
